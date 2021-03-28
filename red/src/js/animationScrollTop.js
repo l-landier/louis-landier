c63384594114aaaa02d1a8255339e09a1bbe4c03
louis landier <landier.louis@gmail.com>
2021-03-28 13:11:14 +0200

    add anchor + text desc

---
+++ red/src/js/animationScrollTop.js
@@ -0,0 +1,31 @@
+export const animationScrollTop = scrollEndElem => {
+  const easeInCubic = t => t * t * t;
+
+  const scrollToElem = (start, stamp, duration, scrollEndElemTop, startScrollOffset) => {
+    const runtime = stamp - start;
+    const progress = runtime / duration;
+    const ease = easeInCubic(progress);
+
+    window.scroll(0, startScrollOffset + scrollEndElemTop * ease);
+
+    if (runtime < duration) {
+      requestAnimationFrame(() => {
+        const newStamp = new Date().getTime();
+        scrollToElem(start, newStamp, duration, scrollEndElemTop, startScrollOffset);
+      });
+    }
+  };
+
+  requestAnimationFrame(() => {
+    const stamp = new Date().getTime();
+    const duration = 1200;
+    const start = stamp;
+    const navHeader = document.getElementById('nav-header');
+    const navHeight = navHeader.clientHeight;
+    const startScrollOffset = window.pageYOffset;
+
+    const scrollEndElemTop = scrollEndElem.getBoundingClientRect().top - navHeight;
+
+    scrollToElem(start, stamp, duration, scrollEndElemTop, startScrollOffset);
+  });
+};
--- red/src/js/index.js
+++ red/src/js/index.js
@@ -1,1 +1,32 @@
-console.log('this js works too');
+import { animationScrollTop } from './animationScrollTop';
+
+export const smoothScroll = () => {
+  const scrollElems = document.getElementsByClassName('smooth-scroll');
+
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < scrollElems.length; i++) {
+    const elem = scrollElems[i];
+
+    elem.addEventListener('click', e => {
+      e.preventDefault();
+      const scrollElemId = e.currentTarget.dataset.scrollTo;
+      const scrollEndElem = document.getElementById(scrollElemId);
+
+      animationScrollTop(scrollEndElem);
+    });
+  }
+};
+
+export const currentAge = () => {
+  const currentAgeBlock = document.getElementById('current-age');
+  let today = new Date();
+  let todayYear = today.getFullYear();
+  let myAge = todayYear - 1990;
+
+  currentAgeBlock.innerText = myAge;
+};
+
+window.addEventListener('load', () => {
+  smoothScroll();
+  currentAge();
+});
